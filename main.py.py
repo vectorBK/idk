2a158aef509e06fcfc963dd3d5531d5d58dba283
vectorBK <133953969+vectorBK@users.noreply.github.com>
2023-11-13 21:29:51 +0330

    Add files via upload

---
+++ main.py.py
@@ -0,0 +1,32 @@
+import subprocess
+
+meta_data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
+data = meta_data.decode('utf8', errors='backslashreplace')
+data = data.split('\n')
+profiles = []
+
+for i in data:
+    if "All User Profile" in i:
+        i = i.split(":")[1].strip()
+        profiles.append(i)
+
+print("{:<30}| {}".format("Wi-Fi Name", 'Password'))
+print("-"*46)
+
+for profiles in profiles:
+    results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', profile, 'key', '=', 'clear'])
+    results = results.decode('utf8', errors='backslashreplace')
+    results = results.split('\n')
+    
+    wifis = [item.split(":")[1].strip() for item in results if "Key Content" in item]
+    
+    # for item in results:
+    #     if "Key Content" in item:
+    #          wifis.append(item.split(":")[1].strip())
+
+    try:
+        print("{:<30}| {}".format(profile, wifis[0]))
+    except:
+        print("{:<30}| {}".format(profile, ""))
+
+
